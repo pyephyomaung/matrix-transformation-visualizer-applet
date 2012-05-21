589ef26c96a4cb71c25e67dca1341b60f644faa3
Pye Phyo Maung <maungp@ripon.edu>
2012-05-21 12:40:43 -0500

    Add affine transformation with gui

--- RipMath/src/RipMathApplet.java
+++ RipMath/src/RipMathApplet.java
@@ -1,6 +1,3 @@
-import static java.lang.Math.PI;
-import static java.lang.Math.cos;
-import static java.lang.Math.sin;
 import static org.math.array.DoubleArray.increment;
 
 import java.awt.Color;
@@ -10,18 +7,17 @@ import net.miginfocom.swing.MigLayout;
 
 // JMathPlot
 import org.math.plot.Plot3DPanel;
-import org.math.plot.plots.BoxPlot3D;
+import org.math.plot.canvas.PlotCanvas;
 import org.math.plot.plots.GridPlot3D;
-import org.math.plot.plots.Plot;
-import org.math.plot.plots.ScatterPlot;
 
-import Jama.EigenvalueDecomposition;
-import Jama.Matrix;
+import delaunay_triangulation.Delaunay_Triangulation;
+import delaunay_triangulation.Point_dt;
 
 // JMathArray
 import static org.math.array.LinearAlgebra.*;
-
+import Jama.EigenvalueDecomposition;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
@@ -34,14 +30,13 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.JTextArea;
 import javax.swing.JPanel;
-import java.awt.BorderLayout;
-import java.awt.Dialog;
+
+import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
-import java.awt.TrayIcon.MessageType;
 import java.util.HashSet;
-import java.util.Random;
 import java.util.Set;
 import java.util.StringTokenizer;
 
@@ -58,14 +53,16 @@ public class RipMathApplet extends JApplet {
 	private double[][] data;
 	private Color[] colorMap;
 	private static Parser psr = new Parser();
-	private JTable LinearTransformationMatrix;
+	private JTable TransformationMatrixTable;
 	private double[][] transformationMatrix;
 	private JButton button_transform;
 	private JTextArea textArea_info;
 	private JPanel panel;
 	private JComboBox comboBox_surfaces;
+	private JComboBox comboBox_transform;
+	private Delaunay_Triangulation delaunay;
 	
-	public String[] surfaces = new String[] {"Plane", "Cube", "Sphere"};
+	public final String[] surfaces = new String[] {"Plane", "Cube", "Sphere"};
 
 	/**
 	 * Create the applet.
@@ -73,7 +70,7 @@ public class RipMathApplet extends JApplet {
 	public RipMathApplet() {
 		// set layout of the applet
 		getContentPane().setLayout(
-				new MigLayout("", "[][200.00px,grow][grow]", "[][200.00,grow,top][200.00]"));
+				new MigLayout("", "[][200.00px,grow][grow][]", "[][200.00,grow,top][200.00,grow]"));
 		
 		comboBox_surfaces = new JComboBox();
 		comboBox_surfaces.setModel(new DefaultComboBoxModel(surfaces));
@@ -85,9 +82,9 @@ public class RipMathApplet extends JApplet {
 				int choice = comboBox_surfaces.getSelectedIndex();
 				switch(choice)
 				{
-				case 0:	data = getPlane(); break;
-				case 1:	data = getCube(); break;
-				case 2: data = getSphere(); break;
+				case 0:	data = generatePlane(); break;
+				case 1:	data = generateCube(); break;
+				case 2: data = generateSphere(); break;
 				}
 				
 				colorMap = mapColor(data);
@@ -102,45 +99,103 @@ public class RipMathApplet extends JApplet {
 		panel = new JPanel();
 		getContentPane().add(panel, "cell 1 2 2 1,grow");
 		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.columnWidths = new int[] { 200, 0, 0 };
+		gbl_panel.columnWidths = new int[] { 200, 0, 0, 0 };
 		gbl_panel.rowHeights = new int[] { 0, 0, 0, 24 };
-		gbl_panel.columnWeights = new double[] { 0.0, 0.0, Double.MIN_VALUE };
+		gbl_panel.columnWeights = new double[] { 0.0, 0.0, 0.0, Double.MIN_VALUE };
 		gbl_panel.rowWeights = new double[] { 0.0, 0.0, 0.0, Double.MIN_VALUE };
 		panel.setLayout(gbl_panel);
 
-		JComboBox comboBox_Transform = new JComboBox();
-		comboBox_Transform.setModel(new DefaultComboBoxModel(new String[] {
+		comboBox_transform = new JComboBox();
+		comboBox_transform.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int choice = comboBox_transform.getSelectedIndex();
+				if(choice == 0)
+				{
+					TransformationMatrixTable.setValueAt("0", 0, 3);
+					TransformationMatrixTable.setValueAt("0", 1, 3);
+					TransformationMatrixTable.setValueAt("0", 2, 3);
+					TransformationMatrixTable.setValueAt("0", 3, 0);
+					TransformationMatrixTable.setValueAt("0", 3, 1);
+					TransformationMatrixTable.setValueAt("0", 3, 2);
+					TransformationMatrixTable.setValueAt("1", 3, 3);
+				}
+			
+				TransformationMatrixTable.repaint();
+			}
+		});
+		
+		comboBox_transform.setModel(new DefaultComboBoxModel(new String[] {
 				"LINEAR", "AFFINE" }));
 		GridBagConstraints gbc_comboBox_Transform = new GridBagConstraints();
 		gbc_comboBox_Transform.anchor = GridBagConstraints.NORTHWEST;
 		gbc_comboBox_Transform.insets = new Insets(0, 0, 5, 5);
 		gbc_comboBox_Transform.gridx = 0;
 		gbc_comboBox_Transform.gridy = 0;
-		panel.add(comboBox_Transform, gbc_comboBox_Transform);
+		panel.add(comboBox_transform, gbc_comboBox_Transform);
 
-		LinearTransformationMatrix = new JTable();
-		LinearTransformationMatrix.setBorder(null);
-		LinearTransformationMatrix.setModel(new DefaultTableModel(
-				new Object[][] { { "1", "0", "0" }, { "0", "1", "0" },
-						{ "0", "0", "1" }, }, new String[] { "", "", "" }) {
-			Class[] columnTypes = new Class[] { String.class, String.class,
-					String.class };
-
+		TransformationMatrixTable = new JTable(){
+			public boolean isCellEditable(int row, int column)
+			{
+				int choice = comboBox_transform.getSelectedIndex();
+				if(choice == 0)
+				{
+					if((row == 0 && column == 3) ||
+						(row == 1 && column == 3) ||
+						(row == 2 && column == 3) ||
+						(row == 3 && column == 0) ||
+						(row == 3 && column == 1) ||
+						(row == 3 && column == 2) ||
+						(row == 3 && column == 3))
+					{
+						return false;
+					}
+				}
+				return true;
+			}
+		};
+		
+		TransformationMatrixTable.setCellSelectionEnabled(true);
+		TransformationMatrixTable.setBorder(null);
+		TransformationMatrixTable.setModel(new DefaultTableModel(
+			new Object[][] {
+				{"1", "0", "0", "0"},
+				{"0", "1", "0", "0"},
+				{"0", "0", "1", "0"},
+				{"0", "0", "0", "1"},
+			},
+			new String[] {
+				"", "", "", ""
+			}
+		) {
+			Class[] columnTypes = new Class[] {
+				String.class, String.class, String.class, String.class
+			};
 			public Class getColumnClass(int columnIndex) {
 				return columnTypes[columnIndex];
 			}
 		});
-
-		GridBagConstraints gbc_LinearTransformationMatrix = new GridBagConstraints();
-		gbc_LinearTransformationMatrix.insets = new Insets(0, 0, 5, 5);
-		gbc_LinearTransformationMatrix.anchor = GridBagConstraints.SOUTHWEST;
-		gbc_LinearTransformationMatrix.gridx = 0;
-		gbc_LinearTransformationMatrix.gridy = 1;
-		panel.add(LinearTransformationMatrix, gbc_LinearTransformationMatrix);
+		TransformationMatrixTable.getColumnModel().getColumn(0).setPreferredWidth(45);
+		TransformationMatrixTable.getColumnModel().getColumn(1).setPreferredWidth(45);
+		TransformationMatrixTable.getColumnModel().getColumn(2).setPreferredWidth(45);
+		TransformationMatrixTable.getColumnModel().getColumn(3).setPreferredWidth(45);
+		
+		TransformationMatrixTable.getColumnModel().getColumn(0).setCellRenderer(new ColoredCellRenderer());
+		TransformationMatrixTable.getColumnModel().getColumn(1).setCellRenderer(new ColoredCellRenderer());
+		TransformationMatrixTable.getColumnModel().getColumn(2).setCellRenderer(new ColoredCellRenderer());
+		TransformationMatrixTable.getColumnModel().getColumn(3).setCellRenderer(new ColoredCellRenderer());
+		
+		GridBagConstraints gbc_TransformationMatrixTable = new GridBagConstraints();
+		gbc_TransformationMatrixTable.insets = new Insets(0, 0, 5, 5);
+		gbc_TransformationMatrixTable.anchor = GridBagConstraints.SOUTHWEST;
+		gbc_TransformationMatrixTable.gridx = 0;
+		gbc_TransformationMatrixTable.gridy = 1;
+		panel.add(TransformationMatrixTable, gbc_TransformationMatrixTable);
+		
 		// initialize gui
 		button_transform = new JButton("Transform");
 		GridBagConstraints gbc_button_transform = new GridBagConstraints();
-		gbc_button_transform.insets = new Insets(0, 0, 5, 5);
+		gbc_button_transform.insets = new Insets(0, 0, 0, 5);
 		gbc_button_transform.gridx = 0;
 		gbc_button_transform.gridy = 2;
 		panel.add(button_transform, gbc_button_transform);
@@ -163,20 +218,46 @@ public class RipMathApplet extends JApplet {
 
 		// testing with 45 degree rotation
 		transformationMatrix = new double[][] { 
-				{ 1, 0, 0 },
-				{ 0, 1, 0 }, 
-				{ 0, 0, 1 } };
+				{ 1, 0, 0, 0 },
+				{ 0, 1, 0, 0 }, 
+				{ 0, 0, 1, 0 },
+				{ 0, 0, 0, 1 }};
 
 		// create your PlotPanel (you can use it as a JPanel) with a legend at SOUTH
 		inplot = new Plot3DPanel("SOUTH");
-		outplot = new Plot3DPanel("SOUTH");
+		inplot.setPreferredSize(new Dimension(300,350));
+		inplot.getAxis(0).setColor(Color.BLUE);
+		inplot.getAxis(1).setColor(Color.RED);
 		
-		getContentPane().add(inplot, "cell 0 1 2 1,grow");
-		getContentPane().add(outplot, "cell 2 1,grow");
+		outplot = new Plot3DPanel("SOUTH");
+		outplot.setPreferredSize(new Dimension(300,350));
+		outplot.getAxis(0).setColor(Color.BLUE);
+		outplot.getAxis(1).setColor(Color.RED);
+		
+		delaunay = new Delaunay_Triangulation();
+		
+		getContentPane().add(inplot, "cell 0 1 2 1,alignx center,aligny center");
+		getContentPane().add(outplot, "cell 2 1,alignx center,aligny center");
 		comboBox_surfaces.setSelectedIndex(0);
 	}
 	
-	private double[][] getSphere() {
+	static class ColoredCellRenderer extends DefaultTableCellRenderer {
+		@Override
+		public Component getTableCellRendererComponent(JTable table,
+				Object value, boolean isSelected, boolean hasFocus, int row,
+				int column) {
+
+			super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+			if (table.isCellEditable(row, column)) {
+				setBackground(Color.WHITE);
+			} else {
+				setBackground(Color.GRAY);
+			}
+			return this;
+		}
+	}
+	
+	private double[][] generateSphere() {	
 		int numPoints = 5000;
 	    double[][] points = new double[numPoints][3];
 	    double inc = Math.PI * (3 - Math.sqrt(5));
@@ -192,22 +273,22 @@ public class RipMathApplet extends JApplet {
 	 
 	        points[k] = new double[] {x, y, z};
 	    }
-	 
+				
 	    return points;
 	}
 	
-	public double[][] getPlane()
+	public double[][] generatePlane()
 	{
 		// define your data
-		double[] x = increment(-10.0, 0.5, 10.0);
-		double[] y = increment(-10.0, 0.5, 10.0);
+		double[] x = increment(-5.0, 0.2, 5.0);
+		double[] y = increment(-5.0, 0.2, 5.0);
 		double[][] z = f(x, y);
 				
 		GridPlot3D plane = new GridPlot3D("", Color.BLACK, x, y, z);
 		return plane.getData();
 	}
 	
-	public double[][] getCube()
+	public double[][] generateCube()
 	{
 		double[] x = increment(-0.5, 0.05, 0.5);
 	    double[] y = increment(-0.5, 0.05, 0.5);
@@ -275,19 +356,28 @@ public class RipMathApplet extends JApplet {
 		try
 		{
 			// Update transformation matrix
-			for (int i = 0; i < 3; i++) {
-				for (int j = 0; j < 3; j++) {
-					String expression = (String) LinearTransformationMatrix
-							.getValueAt(i, j);
-					
-						double d = parseAndEvaluate(expression);
-						transformationMatrix[i][j] = d;
+			for (int i = 0; i < 4; i++) 
+			{
+				for (int j = 0; j < 4; j++) 
+				{
+					String expression = (String) TransformationMatrixTable.getValueAt(i, j);
+					double d = parseAndEvaluate(expression);
+					transformationMatrix[i][j] = d;
 				}
 			}
 			
+			// double start = System.nanoTime();	// debug
+			
+			// append dummy column vector
+			double[][] points4D = insertColumns(data, 3, one(data.length));
+			
 			// Get transformed points
-			double[][] transformedData = linearTransform(data, transformationMatrix);
-
+			double[][] transformedData = transform(points4D, transformationMatrix);
+			
+			//double end = System.nanoTime();
+			//System.out.println(end-start);
+			
+	
 			// plot the transformed (output) data
 			outplot.removeAllPlots();	// remove the current output plot
 			scatterPlot_out = new CustomizedScatteredPlot("transformed", colorMap,
@@ -302,7 +392,18 @@ public class RipMathApplet extends JApplet {
 			JOptionPane.showMessageDialog(this, "Invalid expression!");
 		}
 	}
+	
+	// Actual Transformation
+	public static double[][] transform(double[][] points4D, double[][] transformationMatrix) {
+		double[][] result = new double[4][points4D.length];
 
+		// matrix multiplication (batch multiplication => much faster)
+		result = times(transformationMatrix, transpose(points4D));
+
+		// remove dummy components and return only 3d points
+		return deleteColumns(transpose(result), 3);
+	}
+	
 	/**
 	 * Update information on determinant, eigen value and rank Called on clicked
 	 * of button_Transform
@@ -335,7 +436,7 @@ public class RipMathApplet extends JApplet {
 
 		double[] xCoords = getColumnCopy(points, 0); // get the x coordinates
 		double[] yCoords = getColumnCopy(points, 1); // get the y coordinates
-		double[] zCoords = getColumnCopy(points, 2); // get the z coordinates
+		//double[] zCoords = getColumnCopy(points, 2); // get the z coordinates
 
 		double xMin = min(xCoords);
 		double xMax = max(xCoords);
@@ -344,15 +445,15 @@ public class RipMathApplet extends JApplet {
 		double yMin = min(yCoords);
 		double yMax = max(yCoords);
 		double yRange = yMax - yMin;
-
-		// double zMin = min(zCoords);
-		// double zMax = max(zCoords);
-		// double zRange = zMax - zMin;
-
+		/*
+		 double zMin = min(zCoords);
+		 double zMax = max(zCoords);
+		 double zRange = zMax - zMin;
+		 */
 		for (int i = 0; i < points.length; i++) {
 			int R = (int) ((255 / xRange) * (points[i][0] - xMin));
 			int G = (int) ((255 / yRange) * (points[i][1] - yMin));
-			// int B = (int) ((255/zRange) * (points[i][2] - zMin));
+			//int B = (int) ((255/zRange) * (points[i][2] - zMin));
 			colorMap[i] = new Color(R, G, 128);
 		}
 
@@ -373,18 +474,6 @@ public class RipMathApplet extends JApplet {
 				z[j][i] = f1(x[i], y[j]);
 		return z;
 	}
-
-	// linear transformation
-	public static double[][] linearTransform(double[][] points,
-			double[][] transformationMatrix) {
-		double[][] result = new double[points.length][3];
-
-		for (int i = 0; i < points.length; i++) {
-			result[i] = times(transformationMatrix, points[i]);
-		}
-
-		return result;
-	}
 	
 	// print array
 	public static String print(double[] array, String separator)
@@ -398,13 +487,13 @@ public class RipMathApplet extends JApplet {
 		return s;
 	}
 	
-	public static String print(double[] array)
+	public static String format(double[] array)
 	{
 		return print(array, " ");
 	}
 
 	// print matrix
-	public static String print(double[][] matrix) {
+	public static String format(double[][] matrix) {
 		String s = "";
 		for (double[] row : matrix) 
 		{
